3422040490cf736cd6d9ec2a93cee15a7c3c5351
Aryan Kothari <87589047+thearyadev@users.noreply.github.com>
2024-06-05 18:48:07 -0400

    adjust test screen

--- src/app/home/page.tsx
+++ src/app/home/page.tsx
@@ -6,15 +6,16 @@ import { Button } from "~/components/ui/button";
 import { api } from "~/trpc/react";
 
 export default function Home() {
-  const { data } = api.library.allSongs.useQuery({ page: 1, pageSize: 88888 });
-  const {setTrack} = useContext(PlayerContext)
+  const { data } = api.library.allSongs.useQuery({ page: 1, pageSize: 50 });
+  console.log(data)
+  const {changeTrack} = useContext(PlayerContext)
   return (
-    <div className="scroll-auto">
+    <div className="overflow-y-scroll h-screen">
       {data?.map((item) => {
         return (
           <div className="text-white">
             {item.name} by {item.artistNames}
-            <Button onClick={() => {setTrack(item)}}>play</Button>
+            <Button onClick={() => {changeTrack(item)}}>play</Button>
           </div>
         );
       })}
