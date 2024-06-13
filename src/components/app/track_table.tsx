a3ef3970e6e7badb75e9d3cfb44f7c062864a707
Aryan Kothari <87589047+thearyadev@users.noreply.github.com>
2024-06-13 16:26:19 -0400

    add non-paginated table

--- src/components/app/track_table.tsx
+++ src/components/app/track_table.tsx
@@ -19,6 +19,62 @@ type TrackTableScrollPaginatedProps = {
   pageSize: number;
   page: number;
 };
+type TrackTableScroll = {
+  tracks: TrackQuery[];
+};
+
+function TrackCell({
+  track,
+  clickFn,
+  index,
+}: {
+  track: TrackQuery;
+  clickFn: (track: TrackQuery) => void;
+  index: number;
+}) {
+  return (
+    <div
+      className="grid grid-cols-12 grid-rows-1 gap-4 p-3 hover:bg-zinc-700 "
+      onMouseDown={() => clickFn(track)}
+    >
+      <div className="col-span-6 flex flex-row space-x-3 text-sm">
+        <div>
+          <img
+            src={`/api/covers?id=${track!.id}]`}
+            className="h-10 w-10 rounded-md"
+            loading={index <= 20 ? "eager" : "lazy"}
+          />
+        </div>
+        <div>
+          <div>{track!.name}</div>
+          <div className="">
+            {track!.artistIds.split(";").map((artistId, index) => (
+              <Link
+                href={`/artist/${artistId}`}
+                className="text-xs text-gray-500 transition-all fade-in-100 fade-out-100 hover:text-orange-400 "
+              >
+                {track!.artistNames.split(";")[index]}
+                {index < track!.artistIds.split(";").length - 1 ? ", " : ""}
+              </Link>
+            ))}
+          </div>
+        </div>
+      </div>
+      <div className="col-span-3 content-center">
+        <Link
+          href={`/album/${track!.albumId}`}
+          className="text-xs text-gray-500 transition-all fade-in-100 fade-out-100 hover:text-orange-400"
+        >
+          {track!.albumName}
+        </Link>
+      </div>
+      <div className="col-span-2 content-center text-xs text-gray-500">
+        {secondsToTimeString(track!.duration as number)}
+      </div>
+    </div>
+  );
+}
+
 function secondsToTimeString(seconds: number) {
   var minutes = Math.floor(seconds / 60);
   var remainingSeconds = Math.floor(seconds % 60);
@@ -27,22 +83,23 @@ function secondsToTimeString(seconds: number) {
     minutes + ":" + (remainingSeconds < 10 ? "0" : "") + remainingSeconds;
   return timeString;
 }
-export function TrackTableScrollPaginatedU(
+export function TrackTableScrollPaginated(
   props: TrackTableScrollPaginatedProps,
 ) {
   const heightRef = useRef<number>(0);
   const [tracks, setTracks] = useState<TrackQuery[]>(props.initialTracks);
   const [page, setPage] = useState(props.page + 1);
   const { changeTrack } = useTrack()!;
-  const { addTrackPrevious } = useQueue()!;
+  const { addTrackPrevious, addTrack } = useQueue()!;
   const { track: currentTrack } = useTrack()!;
   const { data } = api.library.allSongs.useQuery({
     page: page,
     pageSize: props.pageSize,
   });
   const handleTrackSwitch = (track: TrackQuery) => {
-    if (currentTrack) addTrackPrevious(currentTrack);
-    changeTrack(track, true);
+    // if (currentTrack) addTrackPrevious(currentTrack);
+    // changeTrack(track, true);
+    addTrack(track)
   };
 
   const handleScroll = (currentPosition: number, maxPosition: number) => {
@@ -56,7 +113,6 @@ export function TrackTableScrollPaginatedU(
   useEffect(() => {
     if (data) setTracks((prevTracks) => [...prevTracks, ...data]);
   }, [data]);
-  console.log("rendah");
 
   return (
     <>
@@ -78,52 +134,44 @@ export function TrackTableScrollPaginatedU(
       >
         {tracks.map((track, index) => {
           return (
-            <div
-              className="grid grid-cols-12 grid-rows-1 gap-4 p-3 hover:bg-zinc-700 "
-              onMouseDown={() => handleTrackSwitch(track)}
-            >
-              <div className="col-span-6 flex flex-row space-x-3 text-sm">
-                <div>
-                  <img
-                    src={`/api/covers?id=${track!.id}]`}
-                    className="h-10 w-10 rounded-md"
-                    loading={index <= 20 ? "eager" : "lazy"}
-                  />
-                </div>
-                <div>
-                  <div>{track!.name}</div>
-                  <div className="">
-                    {track!.artistIds.split(";").map((artistId, index) => (
-                      <Link
-                        href={`/artist/${artistId}`}
-                        className="text-xs text-gray-500 transition-all fade-in-100 fade-out-100 hover:text-orange-400 "
-                      >
-                        {track!.artistNames.split(";")[index]}
-                        {index < track!.artistIds.split(";").length - 1
-                          ? ", "
-                          : ""}
-                      </Link>
-                    ))}
-                  </div>
-                </div>
-              </div>
-              <div className="col-span-3 content-center">
-                <Link
-                  href={`/album/${track!.albumId}`}
-                  className="text-xs text-gray-500 transition-all fade-in-100 fade-out-100 hover:text-orange-400"
-                >
-                  {track!.albumName}
-                </Link>
-              </div>
-              <div className="col-span-2 content-center text-xs text-gray-500">
-                {secondsToTimeString(track!.duration as number)}
-              </div>
-            </div>
+            <TrackCell
+              track={track}
+              clickFn={handleTrackSwitch}
+              index={index}
+            />
           );
         })}
       </ScrollArea>
     </>
   );
 }
+export function TrackTableScroll(props: TrackTableScroll) {
+  const { changeTrack } = useTrack()!;
+  const { addTrackPrevious } = useQueue()!;
+  const handleTrackSwitch = (track: TrackQuery) => {
+    if (currentTrack) addTrackPrevious(currentTrack);
+    changeTrack(track, true);
+  };
 
-export const TrackTableScrollPaginated = memo(TrackTableScrollPaginatedU);
+  return (
+    <>
+      <div className="grid grid-cols-12 grid-rows-1 gap-4 p-3  text-gray-500">
+        <div className="col-span-6 text-xs">TRACK</div>
+        <div className="col-span-3 text-xs">ALBUM</div>
+        <div className="col-span-2 text-xs">TIME</div>
+      </div>
+      <Separator />
+      <ScrollArea className="h-full w-full rounded-md">
+        {props.tracks.map((track, index) => {
+          return (
+            <TrackCell
+              track={track}
+              clickFn={handleTrackSwitch}
+              index={index}
+            />
+          );
+        })}
+      </ScrollArea>
+    </>
+  );
+}
