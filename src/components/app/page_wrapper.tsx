bbabf534fcd26209bca11e07ecc28d1b3df0255d
Aryan Kothari <87589047+thearyadev@users.noreply.github.com>
2024-06-12 22:00:41 -0400

    add components for use on all pages: - wrapper to handle page body - heading

---
+++ src/components/app/page_wrapper.tsx
@@ -0,0 +1,9 @@
+export default function PageWrapper({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return <div className="h-screen w-full pb-20 text-white">
+    {children}
+  </div>;
+}
