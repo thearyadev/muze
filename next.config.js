f5f41d7e8edde327477075610b9949009a75c24d
Aryan Kothari <87589047+thearyadev@users.noreply.github.com>
2024-08-05 10:29:02 -0400

    chore: disable next build eslint verification

--- next.config.js
+++ next.config.js
@@ -7,6 +7,9 @@ await import('./src/env.js')
 /** @type {import("next").NextConfig} */
 const config = {
   experimental: {},
+  eslint: {
+    ignoreDuringBuilds: true,
+  }
 }
 
 export default config
