b50f7a9183ba47c09734b362b24d42ab037e7410
Aryan Kothari <87589047+thearyadev@users.noreply.github.com>
2024-08-04 22:52:14 -0400

    fix(db): ensure migrated

--- src/server/db/index.ts
+++ src/server/db/index.ts
@@ -1,26 +1,5 @@
-// import Database from "better-sqlite3";
-// import { drizzle } from "drizzle-orm/better-sqlite3";
-// import { migrate } from "drizzle-orm/better-sqlite3/migrator";
-
-// import { env } from "~/env";
-// import * as schema from "./schema";
-
-// /**
-//  * Cache the database connection in development. This avoids creating a new connection on every HMR
-//  * update.
-//  */
-// const globalForDb = globalThis as unknown as {
-//   conn: Database.Database | undefined;
-// };
-
-// export const conn =
-//   globalForDb.conn ?? new Database(env.DATABASE_URL, { fileMustExist: false });
-// if (env.NODE_ENV !== "production") globalForDb.conn = conn;
-
-// export const db = drizzle(conn, { schema });
-// migrate(db, { migrationsFolder: "./drizzle" });
-
 import { drizzle } from "drizzle-orm/postgres-js";
+import { migrate } from "drizzle-orm/postgres-js/migrator";
 import postgres from "postgres";
 
 import { env } from "~/env";
@@ -42,3 +21,4 @@ const conn =
 if (env.NODE_ENV !== "production") globalForDb.conn = conn;
 
 export const db = drizzle(conn, { schema });
+migrate(db, { migrationsFolder: "./drizzle" });
