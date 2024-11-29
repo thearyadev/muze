a4484692bc6fe34cbb9b002d1afafe38a50b3834
Aryan Kothariset -g allow-passthrough on <87589047+thearyadev@users.noreply.github.com>
2024-11-29 14:20:05 -0500

    fix: ts compliance

--- src/app/app/albums/[albumId]/page.tsx
+++ src/app/app/albums/[albumId]/page.tsx
@@ -1,31 +1,30 @@
 import Image from 'next/image'
 import PageHeading from '~/components/app/page_heading'
 import { TrackTableScroll } from '~/components/app/track_table'
-import { api } from '~/trpc/server'
 
 export default async function Home({
   params,
 }: {
   params: { albumId: string }
 }) {
-  const albumQuery = await api.library.albumDetail(params.albumId)
-  const tracks = albumQuery?.tracks || []
-  return (
-    <>
-      <PageHeading>
-        <div className="flex flex-row items-center">
-          <Image
-            alt={albumQuery?.album?.name || 'Album Cover'}
-            src={`/api/covers?id=${tracks[0]?.id}&size=xl`}
-            className="mr-5 h-24 w-24 rounded-md"
-            loading="eager"
-            width={40}
-            height={40}
-          />
-          {albumQuery?.album?.name}
-        </div>
-      </PageHeading>
-      <TrackTableScroll tracks={tracks} />
-    </>
-  )
+  // const albumQuery = await api.library.albumDetail(params.albumId)
+  // const tracks = albumQuery?.tracks || []
+  // return (
+  //   <>
+  //     <PageHeading>
+  //       <div className="flex flex-row items-center">
+  //         <Image
+  //           alt={albumQuery?.album?.name || 'Album Cover'}
+  //           src={`/api/covers?id=${tracks[0]?.id}&size=xl`}
+  //           className="mr-5 h-24 w-24 rounded-md"
+  //           loading="eager"
+  //           width={40}
+  //           height={40}
+  //         />
+  //         {albumQuery?.album?.name}
+  //       </div>
+  //     </PageHeading>
+  //     <TrackTableScroll tracks={tracks} />
+  //   </>
+  // )
 }
