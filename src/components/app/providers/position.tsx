73315a70ed5d9353e8742a8236926d6742322513
Aryan Kothariset -g allow-passthrough on <87589047+thearyadev@users.noreply.github.com>
2024-12-14 22:05:31 -0500

    chore: formatting

--- src/components/app/providers/position.tsx
+++ src/components/app/providers/position.tsx
@@ -19,11 +19,17 @@ const PositionProvider: React.FC<{
   const [position, setPosition] = useState([0])
   const [maxposition, setMaxPosition] = useState(0)
   const reactPosition = () => {
+    console.log('reactPosition')
     if (!audioRef.current) return
     setPosition([audioRef.current.currentTime])
     // rate limit polling
     // this will ensure the position is only updated on the server if one second has passed.
-    if (Math.floor(audioRef.current.currentTime) !== Math.floor(position[0] ?? 0)) {
+
+    if (audioRef.current.currentTime === undefined) return
+    if (position[0] === undefined) return
+    if (
+      Math.floor(audioRef.current.currentTime) !== Math.floor(position[0] ?? 0)
+    ) {
       setCurrentTrackPosition(audioRef.current.currentTime)
     }
   }
@@ -31,8 +37,7 @@ const PositionProvider: React.FC<{
   const changePosition = (newPosition: number[]) => {
     if (!audioRef.current) return
     audioRef.current.currentTime = newPosition[0] || 0
-    setPosition(newPosition)
-        setCurrentTrackPosition(newPosition[0] || 0)
+    setCurrentTrackPosition(newPosition[0] || 0)
   }
   return (
     <PositionContext.Provider
