1fb037a7ca379675e165ce6ce031194601ebcfef
Aryan Kothariset -g allow-passthrough on <87589047+thearyadev@users.noreply.github.com>
2025-01-16 17:12:55 -0500

    feat: pwa

---
+++ src/app/manifest.ts
@@ -0,0 +1,15 @@
+import type { MetadataRoute } from 'next'
+ 
+export default function manifest(): MetadataRoute.Manifest {
+  return {
+    name: 'Muze',
+    short_name: 'Muze',
+    description: 'Music Player',
+    start_url: '/',
+    display: 'standalone',
+    background_color: '#ffffff',
+    theme_color: '#000000',
+    icons: [
+    ],
+  }
+}
