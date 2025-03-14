a66e1df60c527dc52197d77ca9d0afcf4f7f5bec
Aryan Kothariset -g allow-passthrough on <87589047+thearyadev@users.noreply.github.com>
2025-03-14 10:12:42 -0400

    fix: link href

---
+++ src/app/app/artists/[artistId]/page.tsx
@@ -0,0 +1,32 @@
+import Image from 'next/image'
+import { notFound, redirect } from 'next/navigation'
+import PageHeading from '~/components/app/page_heading'
+import { TrackTable } from '~/components/app/track_table'
+import { getArtist, getArtistTracks } from '~/lib/actions/library'
+
+export default async function ArtistPage({
+  params,
+}: { params: Promise<{ artistId: string }> }) {
+  const artist = await getArtist((await params).artistId)
+  if (!artist.content) {
+    return notFound()
+  }
+
+  const tracks = await getArtistTracks(artist.content.id)
+  if (!tracks.content) {
+    console.log('no content')
+    return notFound()
+  }
+  if (!tracks.content.length) {
+    console.log('no tracks')
+    return notFound()
+  }
+  return (
+    <>
+      <PageHeading>
+        <div className="flex flex-row items-center">{artist.content.name}</div>
+      </PageHeading>
+      <TrackTable tracks={tracks.content} pageSize={50} page={1} toolbar />
+    </>
+  )
+}
