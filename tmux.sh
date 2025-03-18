2836c89db5e2d674e2d53e224feec84d5b69c07b
Aryan Kothariset -g allow-passthrough on <87589047+thearyadev@users.noreply.github.com>
2025-03-18 11:10:25 -0400

    fix(deps): update drizzle-orm
    adds commnads to tmux.sh to start drizzle-kit studio

--- tmux.sh
+++ tmux.sh
@@ -21,13 +21,21 @@ else
 	docker kill muze-db 2>/dev/null
 	docker rm muze-db 2>/dev/null
 	tmux send-keys -t $SESSION_NAME:2.2 "./start_database.sh" Enter
+	sleep 1
+	tmux send-keys -t $SESSION_NAME:2.1 "pnpm drizzle-kit migrate" Enter
 	tmux send-keys -t $SESSION_NAME:2.1 "pnpm dev" Enter
-
+        tmux split-window -v -t $SESSION_NAME:2.2
+	IP=$(ip addr show ens18 | grep "inet " | awk '{print $2}' | cut -d'/' -f1)
+	tmux send-keys -t $SESSION_NAME:2.3 "pnpm drizzle-kit studio --host $IP" Enter
 
 	tmux new-window -t $SESSION_NAME:3 -n "Manifests"
 	tmux send-keys -t $SESSION_NAME:3.1 "cd ~/manifests/muze" Enter
+	tmux send-keys -t $SESSION_NAME:3.1 "nvim manifest.yaml" Enter
+	tmux split-window -h -t $SESSION_NAME:3.1
+	tmux send-keys -t $SESSION_NAME:3.2 "cd ~/manifests/muze" Enter
 
 	tmux select-window -t $SESSION_NAME:1
+	tmux select-pane -t 1
 	tmux attach-session -t $SESSION_NAME
 fi
 
