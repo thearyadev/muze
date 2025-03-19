91b3ece0c1d8a25b8350aedf2754c2414a947653
Aryan Kothariset -g allow-passthrough on <87589047+thearyadev@users.noreply.github.com>
2025-03-19 11:11:18 -0400

    fix: remove ver from homepage

--- src/app/app/home/page.tsx
+++ src/app/app/home/page.tsx
@@ -1,11 +1,9 @@
 import PageHeading from '~/components/app/page_heading'
-import { env } from '~/env'
 
 export default async function Home() {
   return (
     <>
       <PageHeading>Home</PageHeading>
-      <p>{env.TAG}</p>
     </>
   )
 }
